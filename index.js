3ec633b512dbe7032af48f16238da993ca29f61e
Nora Harris <noraharris@Noras-MacBook-Pro.local>
2019-11-06 19:11:50 -0600

    added initial route

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('hello barreback')
+const express = require('express')
+const app = express()
+
+const PORT = 5000
+
+app.get('/', function (req, res) {
+  res.send('hello barreback')
+})
+
+app.listen(PORT)
