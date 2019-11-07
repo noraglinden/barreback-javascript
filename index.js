1c418836d04c169b635fe470e01bdd7728fded2f
Nora Harris <noraharris@Noras-MacBook-Pro.local>
2019-11-06 20:49:04 -0600

    added nodemon and create exercise route

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ const app = express()
 //Connect to DB
 connectDB()
 
+//Init Middleware
+app.use(express.json({ extended: false }))
+
 const PORT = 5000
 
 // Define Routes
 app.use('/', require('./routes/core'))
+app.use('/exercises', require('./routes/exercises'))
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
