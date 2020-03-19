7962c0a09d0f603ddb7295c30f732caffd7672a2
Nora Harris <noraharris@Noras-MacBook-Pro.local>
2020-03-19 16:44:13 -0400

    simplified creating a class

--- core/classes/createClass.js
+++ core/classes/createClass.js
@@ -1,43 +1,11 @@
-const classTypes = require('../enums/classTypes')
 const classStatuses = require('../enums/classStatuses')
 
 const createClass = req => {
-  const {
-    status,
-    createdAt,
-    classType,
-    warmUp,
-    triceps,
-    weightWork,
-    thigh1,
-    thigh2,
-    thigh3,
-    armsLegs1,
-    armsLegs2,
-    armsLegs3,
-    core,
-    seat1,
-    seat2,
-    coreFocus,
-    abs1,
-    abs2,
-    abs3,
-    backExtension,
-    backDancing,
-  } = req.body
-
   //Build Class Fields
   const classFields = {}
 
-  //Set User Entry Required Fields
-  classFields.classType = classType
-
-  //Set Class Status, default of Created
-  if (status === undefined) {
-    classFields.status = classStatuses.CREATED
-  } else {
-    classFields.status = status
-  }
+  classFields.classType = req.body.classType
+  classFields.status = classStatuses.status.CREATED
 
   return classFields
 }
