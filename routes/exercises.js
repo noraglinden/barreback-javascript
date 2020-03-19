f63a61b05cf5d6bcd4fc0cfcd730b9eaa8f714a7
Nora Harris <noraharris@Noras-MacBook-Pro.local>
2020-03-19 11:33:17 -0400

    added exercise route and exited fn name

--- routes/exercises.js
+++ routes/exercises.js
@@ -2,32 +2,27 @@ const express = require('express')
 const router = express.Router()
 const { createExercise } = require('../core/exercises/createExercise')
 const Exercise = require('../models/Exercise')
-const {
-  createExerciseValidationRules,
-} = require('../core/exercises/exercise.validation')
+const { createExerciseRules } = require('../core/exercises/exercise.validation')
 const { validate } = require('./middleware/validation')
 
-router.get('/', (req, res) => {
-  res.send('exercise route')
+// Get all Exercises
+router.get('/', async (req, res) => {
+  const exercises = await Exercise.find()
+  res.json(exercises)
 })
 
 // Create a new Exercise
 //todo reduce duplicate exercises
-router.post(
-  '/',
-  createExerciseValidationRules(),
-  validate,
-  async (req, res) => {
-    try {
-      const exerciseFields = createExercise(req)
-      newExercise = new Exercise(exerciseFields)
-      await newExercise.save()
-      res.json(newExercise)
-    } catch (err) {
-      console.log(err)
-      res.sendStatus(500).send('Server Error')
-    }
+router.post('/', createExerciseRules(), validate, async (req, res) => {
+  try {
+    const exerciseFields = createExercise(req)
+    newExercise = new Exercise(exerciseFields)
+    await newExercise.save()
+    res.json(newExercise)
+  } catch (err) {
+    console.log(err)
+    res.sendStatus(500).send('Server Error')
   }
-)
+})
 
 module.exports = router
