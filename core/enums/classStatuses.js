62849909c949c3859f3aa7f5689a19862e426f11
Nora Harris <noraharris@Noras-MacBook-Pro.local>
2020-03-19 16:43:49 -0400

    made class statuses more extensible

--- core/enums/classStatuses.js
+++ core/enums/classStatuses.js
@@ -1,5 +1,9 @@
-module.exports = {
+const status = {
   CREATED: 'Created',
   IN_PROGRESS: 'In Progress',
   COMPLETE: 'Planned',
 }
+
+const all = [status.CREATED, status.IN_PROGRESS, status.COMPLETE]
+
+module.exports = { status, all }
